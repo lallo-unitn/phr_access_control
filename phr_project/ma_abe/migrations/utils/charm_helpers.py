8e1bdc2e688231d0135d604d644ae70243e237f2
Riccardo Gennaro <riccardo.gennaro@studenti.unitn.it>
2024-10-27 19:34:05 +0100

    added user api server side

--- phr_project/ma_abe/migrations/utils/charm_helpers.py
+++ phr_project/ma_abe/migrations/utils/charm_helpers.py
@@ -1,3 +1,5 @@
+from attr.validators import instance_of
+from charm.core.math.pairing import GT
 from charm.schemes.abenc.abenc_maabe_rw15 import MaabeRW15
 from charm.toolbox.pairinggroup import PairingGroup
 import ma_abe.migrations.utils.constants as const
@@ -9,11 +11,49 @@ class CharmMAABEHelper:
         self.__key_pairs = {}
         self.__group = PairingGroup(const.PAIRING_GROUP)
         self.__ma_abe = MaabeRW15(self.__group)
+        self.__public_keys = {}
+
+        # Dictionary to store auth names and corresponding attributes
+        self.auth_attrs = {
+            'PHR': ['PATIENT@PHR'],
+            'HOSPITAL': ['DOCTOR@HOSPITAL'],
+            'INSURANCE': ['INSURANCEREP@INSURANCE'],
+            'EMPLOYER': ['EMPLOYER_REP@EMPLOYER'],
+            'HEALTH_CLUB': ['HEALTHCLUBTRAINER@HEALTHCLUB']
+        }
+
+        if self.__public_keys is None:
+            self.setup()
+
+        for auth, attrs in self.auth_attrs.items():
+            (public_key, secret_key) = self.__ma_abe.authsetup(self.__public_parameters, auth)
+            # print(f"Public Key for {auth}: {self.__group.serialize(public_key['egga'])}")
+            # print(f"Public Key for {auth}: {self.__group.serialize(public_key['gy'])}")
+            # print(f"Public Key for {auth}: {self.__group.serialize(secret_key['alpha'])}")
+            # print(f"Public Key for {auth}: {self.__group.serialize(secret_key['y'])}")
+            # print(f"Master Secret Key for {auth}: {secret_key}")
+            self.__key_pairs[auth] = {
+                'public_key': public_key,
+                'secret_key': secret_key
+            }
+            self.__public_keys[auth] = public_key
 
     # returns
     # gp = {'g1': g1, 'g2': g2, 'egg': egg, 'H': H, 'F': F}
     def setup(self):
         self.__public_parameters = self.__ma_abe.setup()
+        # g1_str = self.__group.serialize(self.__public_parameters['g1'])
+        # print(type(g1_str))
+        # print(f"Public Parameters: {g1_str}")
+        # g1 = self.__group.deserialize(g1_str)
+        # print(f"Public Parameters: {g1}")
+        print(f"Public Parameters: {self.__public_parameters}")
+
+    def get_random_group_element(self):
+        return self.__group.random(GT)
+
+    def get_pairing_group(self):
+        return self.__group
 
     # returns
     # pk = {'name': name, 'egga': egga, 'gy': gy} and
@@ -55,10 +95,16 @@ class CharmMAABEHelper:
     # {'policy': policy_str, 'C0': C0, 'C1': C1, 'C2': C2, 'C3': C3, 'C4': C4}
     # NOTE THAT every attribute must be in the form of
     # attr = "%s@%s" % (attribute_name, auth_name)
-    def encrypt(self, policy, msg):
+    def encrypt(self, msg, policy):
         return self.__ma_abe.encrypt(self.__public_parameters, self.__public_keys, msg, policy)
 
     # raises exception when the access policy can not be
     # satisfied with the user's attributes.
     def decrypt(self, user_keys, cipher_text):
         return self.__ma_abe.decrypt(self.__public_parameters, user_keys, cipher_text)
+
+    def unpack_attribute(self, user_attr):
+        return self.__ma_abe.unpack_attribute(user_attr)
+
+    def merge_dicts(self, dict1, dict2):
+        return {**dict1, **dict2}
