ca49c9d9877f059b0312793240c8a2bfa43e8983
Riccardo Gennaro <riccardo.gennaro@studenti.unitn.it>
2024-10-30 17:06:57 +0100

    put dummy data functions for apis in separate package

--- phr_project/accounts/views.py
+++ phr_project/accounts/views.py
@@ -1,6 +1,7 @@
 # accounts/views.py
 from typing import List, Mapping
 
+from accounts.api_dummy_data.dummy import __get_test_enc_messages, __get_test_user_attr
 #from django.shortcuts import render, redirect
 #from django.contrib import messages
 
@@ -102,39 +103,3 @@ def get_user_record(request, uuid: str):
 
     return JsonResponse(enc_record)
 
-
-def __get_test_user_attr():
-    return ['DOCTOR@HOSPITAL', 'PATIENT@PHR_1']
-
-def __get_test_enc_messages():
-    ma_abe_service = MAABEService()
-
-    # String message to encrypt
-    message1 = "This is a secret message"
-    policy1 = '((PATIENT@PHR_1 or DOCTOR@HOSPITAL))'
-    message1_id = "1"
-
-    message2 = "This is another secret message"
-    policy2 = '((PATIENT@PHR_2 or DOCTOR@HOSPITAL))'
-    message2_id = "2"
-
-    enc_message1 = ma_abe_service.encrypt(message1, policy1)
-    enc_message2 = ma_abe_service.encrypt(message2, policy2)
-
-    print(f"enc_message1: {enc_message1}")
-    print(f"enc_message2: {enc_message2}")
-
-    abe_policy_enc_key1 = enc_message1['abe_policy_enc_key']
-    sym_enc_file1 = enc_message1['sym_enc_file']
-
-    abe_policy_enc_key2 = enc_message2['abe_policy_enc_key']
-    sym_enc_file2 = enc_message2['sym_enc_file']
-
-    enc_message1 = {'abe_policy_enc_key': abe_policy_enc_key1, 'sym_enc_file': sym_enc_file1}
-    enc_message2 = {'abe_policy_enc_key': abe_policy_enc_key2, 'sym_enc_file': sym_enc_file2}
-
-    messages: dict = {}
-    messages[message1_id] = enc_message1
-    messages[message2_id] = enc_message2
-
-    return messages
